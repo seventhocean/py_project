01a34132b929988cb0bd5aa6ec87cc1442e78355
gaoyuan <gy17868607565@163.com>
2025-12-09 00:52:24 +0800

    移除元素--左右双指针

---
+++ removeElement.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = len(nums) - 1
+        j = 0
+        
+        while j <= i:
+            if nums[j] == val:
+                nums[j], nums[i] = nums[i], nums[j]
+                i -= 1
+            else:
+                j += 1
+        
+        return i + 1
+    
+# def removeElement(self, nums: List[int], val: int) -> int:
+#     slow = 0
+#     for fast in range(len(nums)):
+#         if nums[fast] != val:
+#             nums[slow] = nums[fast]
+#             slow += 1
+#     return slow
